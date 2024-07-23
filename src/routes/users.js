7e4d59e97d311e936cccac51655fb53d95a28d27
Karpova Julia <am_julia@mail.ru>
2024-07-23 12:56:26 +0600

    patch +

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,7 +13,7 @@ router.use(loggerTwo);
 router.get("/users", getUsers);
 router.get("/users/:user_id", getUser);
 router.post("/users", createUser);
-router.post("/users/:user_id", updateUser);
+router.patch("/users/:user_id", updateUser);
 router.delete("/users/:user_id", deleteUser);
 
 module.exports = router;
